30f8d18df49dea4c4d1e5dc5cdb3911e28b2f14f
TheFimerHub <m.stepanov@it-park.tech>
2023-09-25 18:22:51 +0300

    Initial commit

---
+++ src/channel.py
@@ -0,0 +1,12 @@
+from src.implemented import youtube, printj
+
+
+class Channel:
+    """Класс для ютуб-канала"""
+
+    def __init__(self, channel_id: str) -> None:
+        self.channel_id = channel_id
+
+    def print_info(self) -> None:
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
