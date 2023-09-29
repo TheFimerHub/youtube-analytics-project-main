3444745cb2887dedf467f9d6a33f1e3e57101a8c
TheFimerHub <m.stepanov@it-park.tech>
2023-09-29 18:53:40 +0300

    homework_2_v2

--- src/channel.py
+++ src/channel.py
@@ -15,9 +15,30 @@ class Channel:
         self.title = self._channel["items"][0]["snippet"]["title"]
         self.description = self._channel["items"][0]["snippet"]["description"]
         self.url = "https://www.youtube.com/" + self._channel["items"][0]["snippet"]["customUrl"]
-        self.subscribers = self._channel["items"][0]["statistics"]["subscriberCount"]
+        self.subscribers = int(self._channel["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self._channel["items"][0]["statistics"]["videoCount"]
         self.views = self._channel["items"][0]["statistics"]["viewCount"]
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscribers + other.subscribers
+
+    def __sub__(self, other):
+        return self.subscribers - other.subscribers
+
+    def __lt__(self, other):
+        return self.subscribers < other.subscribers
+
+    def __le__(self, other):
+        return self.subscribers <= other.subscribers
+
+    def __gt__(self, other):
+        return self.subscribers > other.subscribers
+
+    def __ge__(self, other):
+        return self.subscribers >= other.subscribers
 
     @property
     def channel_id(self) -> str:
@@ -43,5 +64,6 @@ class Channel:
         }
         with open(file, 'w', encoding='utf-8') as json_file:
             json.dump(channel_data, json_file, ensure_ascii=False, indent=4)
+
 
 print(Channel)
